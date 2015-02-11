6a712c8023ff961085c23924ef4d0b8029ec238c
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-10 20:20:10 -0500

    Removed example Python script from separate file.

--- manuscripts/pymbar_example.py
+++ manuscripts/pymbar_example.py
@@ -1,4 +1,7 @@
 from pymbar import timeseries
-[t, g, Neff_max] = timeseries.detectEquilibration(A_t) # determine equilibrated region
-A_t_equilibrated = A_t[t:] # extract equilibrated part of the data for analysis
+# determine equilibrated region
+[t, g, Neff_max] = timeseries.detectEquilibration(A_t)
+# extract equilibrated region
+A_t_equilibrated = A_t[t:]
+
 
