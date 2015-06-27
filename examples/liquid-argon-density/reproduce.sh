bd94f4cd07e7b7374df6190bbf484c5d0823af58
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-06-27 14:57:42 -0400

    Updates to manuscript text.

--- examples/liquid-argon-density/reproduce.sh
+++ examples/liquid-argon-density/reproduce.sh
@@ -8,5 +8,7 @@
 # This requires the conda Pythjon package environment management tool.
 
 # Create conda environment with necessary tools.
+conda create --yes -p conda-env python=2.7 openmmtools=0.7.0 openmm=6.2 matplotlib=1.4
+source activate conda-env
 
 #
