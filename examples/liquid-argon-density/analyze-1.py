4a23f939d04d6b6087cc6dcecff70832e442d421
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-06-28 19:03:46 -0400

    Finished initial manuscript draft.

--- examples/liquid-argon-density/analyze-1.py
+++ examples/liquid-argon-density/analyze-1.py
@@ -7,18 +7,41 @@ Analyze multiple replicates of a simulation of liquid argon at constant pressure
 
 import numpy as np
 import os.path
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
+import pylab
+#import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
+
+#from equilibration import statisticalInefficiency_geyer as statisticalInefficiency
+from equilibration import statisticalInefficiency_multiscale as statisticalInefficiency
 
 data_directory = 'data'
+figure_directory = 'figures'
 
 nreplicates = 100
 niterations = 10000
 
+# TODO: Read this in?
+timestep = 0.002846993438 # ps
+nsteps_per_iteration = 25
+iterations_per_ns = timestep * nsteps_per_iteration * 1000 # ns per iteration
+
+#
+# Ensure figure directory exists
+#
+
+if not os.path.exists(figure_directory):
+    print "Creating figures directory..."
+    os.makedirs(figure_directory)
+#
+# READ DATA
+#
+
 reduced_density_it = np.zeros([nreplicates, niterations+1], np.float64)
 reduced_potential_it = np.zeros([nreplicates, niterations+1], np.float64)
 
-
-# Read data
 for replicate in range(nreplicates):
     filename = os.path.join(data_directory, '%05d.output' % replicate)
     lines = open(filename, 'r').readlines()
@@ -27,45 +50,39 @@ for replicate in range(nreplicates):
         reduced_density_it[replicate, iteration] = float(elements[1])
         reduced_potential_it[replicate, iteration] = float(elements[2])
 
-print reduced_density_it
-
-# Plot
-
 A_it = reduced_density_it
 
-# TODO: Read this in?
-timestep = 0.002846993438 # ps
-nsteps_per_iteration = 25
-iterations_per_ns = timestep * nsteps_per_iteration * 1000 # ns per iteration
+# Compute true expectation using mean of second half of all simulations.
+t0 = int(niterations/2)
+true_expectation = A_it[:,t0:].mean(1).mean(0)
+
+# Compute true expectation using all data. # DEBUG
+#true_expectation = A_it.mean().mean()
+#print "true expectation (from all data) : %f" % true_expectation
+
+#
+# PLOT RELAXATION OF REDUCED DENSITIES
+#
 
 # Write initial reduced densities.
 print "Initial reduced densities:"
-print A_it[:,0]
+print A_it[0,0]
 
 x = np.arange(niterations+1) / iterations_per_ns # ns
 A_t = A_it.mean(0)
 dA_t = A_it.std(0)
 
-#print "%8s %8s %8s" % ('t / ps', '<A>', 'd<a>')
-#for t in range(100):
-#    print "%8.1f %8.5f %8.5f" % (t*nsteps_per_iteration*timestep, A_t[t], dA_t[t])
-
 # Save plot to PDF.
-filename = os.path.join('figures', 'argon-density.pdf') # PDF file to write
+filename = os.path.join(figure_directory, 'argon-density.pdf') # PDF file to write
 
-from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages(filename)
-
-import pylab
-#import seaborn as sns
 
 #
 # FIGURE 1
 # Plot average density decay from initial conditions, along with cumulative average and average with some data discarded to equilibrium.
 #
 
-fontsize=10
-
+fontsize = 10
 font = {'family' : 'sans-serif',
         'weight' : 'normal',
         'size'   : fontsize}
@@ -83,22 +100,17 @@ nmax = 4000 # maximum samples to show
 
 subplot = pylab.subplot(211)
 pylab.hold(True)
-#pylab.errorbar(range(niterations+1), A_t, yerr=dA_t)
-#for replicate in range(nreplicates):
-#    pylab.plot(x, A_it[replicate,:])
-#pylab.errorbar(x, A_t, yerr=2*dA_t, fmt='ko')
-#pylab.fill_between(x, A_t+2*dA_t, A_t-2*dA_t, facecolor='grey', alpha=0.5)
+
 pylab.fill_between(x[:nmax:nskip], A_t[:nmax:nskip]+2*dA_t[:nmax:nskip], A_t[:nmax:nskip]-2*dA_t[:nmax:nskip], facecolor='grey', edgecolor='grey', alpha=0.5, linewidth=0)
 pylab.plot(x[:nmax], A_t[:nmax], 'k-')
 
-#pylab.xlabel('simulation time / ns')
 pylab.ylabel(r'reduced density $\rho^*$', fontsize=fontsize)
+
 # Adjust axes.
 oldaxis = pylab.axis()
 pylab.axis([0, x[:nmax].max(), oldaxis[2], oldaxis[3]])
 
 subplot.set_xticks([]) # no x-tick labels
-#subplot.set_yticks([0.7, 0.8, 0.9, 1.0])
 
 #
 # BOTTOM PANEL: CUMULATIVE AVERAGE WITH INITIAL BURN-IN PERIOD DISCARDED
@@ -130,15 +142,6 @@ Acumavg_std_t = Acumavg_it.std(0)
 Aburnin_std_t = Aburnin_it.std(0)
 Arevcum_std_t = Arevcum_it.std(0)
 
-print Arevcum_mean_t
-
-#print "Cumulative average:"
-#print "%8s %8s %8s" % ('t / ps', 'Acum', 'stdAcum')
-#for t in range(100):
-#    print "%8.1f %8.5f %8.5f" % (t*nsteps_per_iteration*timestep, Acumavg_mean_t[t], Acumavg_std_t[t])
-
-true_expectation = Acumavg_mean_t[-1]
-
 # Create a subplot.
 subplot = pylab.subplot(212)
 pylab.subplots_adjust(hspace=0.001)
@@ -147,25 +150,20 @@ pylab.hold(True)
 # Draw shaded confidence intervals
 pylab.fill_between(x[:nmax:nskip], Acumavg_mean_t[:nmax:nskip]+2*Acumavg_std_t[:nmax:nskip], Acumavg_mean_t[:nmax:nskip]-2*Acumavg_std_t[:nmax:nskip], facecolor='red', edgecolor='red', alpha=0.5, linewidth=0)
 pylab.fill_between(x[Nequil:nmax:nskip], Aburnin_mean_t[Nequil:nmax:nskip]+2*Aburnin_std_t[Nequil:nmax:nskip], Aburnin_mean_t[Nequil:nmax:nskip]-2*Aburnin_std_t[Nequil:nmax:nskip], facecolor='blue', edgecolor='blue', alpha=0.5, linewidth=0)
-#pylab.fill_between(x[:nmax:nskip], Arevcum_mean_t[:nmax:nskip]+2*Arevcum_std_t[:nmax:nskip], Arevcum_mean_t[:nmax:nskip]-2*Arevcum_std_t[:nmax:nskip], facecolor='green', edgecolor='green', alpha=0.5, linewidth=0)
 
-pylab.plot([x[0], x[nmax]], [true_expectation, true_expectation], 'k:') # true expectation (from all data)
+pylab.plot([x[0], x[nmax]], [true_expectation, true_expectation], 'k--') # true expectation (from all data)
 pylab.plot(x[:nmax], Acumavg_mean_t[:nmax], 'r-') # cumulative average from beginning
 pylab.plot(x[Nequil:nmax], Aburnin_mean_t[Nequil:nmax], 'b-') # cumulative average after discarding some initial data to equilibrium
-#pylab.plot(x[:nmax], Arevcum_mean_t[:nmax], 'g-')
 
 pylab.legend(['true expectation', 'cumulative average', 'discarding first %d samples to equilibration' % Nequil], fontsize=fontsize, frameon=False)
 
 pylab.xlabel('simulation time / ns', fontsize=fontsize)
 pylab.ylabel(r'reduced density $\rho^*$', fontsize=fontsize)
 
-#subplot.set_yticks([0.7, 0.8, 0.9, 1.0])
-
 # Adjust axes.
 oldaxis = pylab.axis()
 pylab.axis([0, x[:nmax].max(), oldaxis[2], oldaxis[3]])
 
-#pylab.show()
 figure.tight_layout()
 
 # Write figure to PDF
--- examples/liquid-argon-density/analyze-2.py
+++ examples/liquid-argon-density/analyze-2.py
@@ -7,20 +7,51 @@ Analyze multiple replicates of a simulation of liquid argon at constant pressure
 
 import numpy as np
 import os.path
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
+import pylab
+#import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
+
+#from equilibration import statisticalInefficiency_geyer as statisticalInefficiency
+from equilibration import statisticalInefficiency_multiscale as statisticalInefficiency
 
 data_directory = 'data'
+figure_directory = 'figures'
 
 nreplicates = 100
 niterations = 10000
 
-nreplicates = 10 # DEBUG
+#nreplicates = 10
+
+# TODO: Read this in?
+timestep = 0.002846993438 # ps
+nsteps_per_iteration = 25
+iterations_per_ns = timestep * nsteps_per_iteration * 1000 # ns per iteration # TODO: Fix me; inverse of what we want?
+print "iterations_per_ns = %f" % iterations_per_ns
+
+t0max = 1001 # largest number of initial samples to discard
+tmax = 2000 # total trajectory length to include in analysis
+print "tmax = %d (%.3f ns)" % (tmax, tmax / iterations_per_ns)
+
+#
+# Ensure figure directory exists
+#
+
+if not os.path.exists(figure_directory):
+    print "Creating figures directory..."
+    os.makedirs(figure_directory)
+
+#
+# READ DATA
+#
+
+print "Reading data..."
 
 reduced_density_it = np.zeros([nreplicates, niterations+1], np.float64)
 reduced_potential_it = np.zeros([nreplicates, niterations+1], np.float64)
 
-
-# Read data
 for replicate in range(nreplicates):
     filename = os.path.join(data_directory, '%05d.output' % replicate)
     lines = open(filename, 'r').readlines()
@@ -29,79 +60,61 @@ for replicate in range(nreplicates):
         reduced_density_it[replicate, iteration] = float(elements[1])
         reduced_potential_it[replicate, iteration] = float(elements[2])
 
-print reduced_density_it
-
-# Extarct observable.
+# Extract observables.
 
 A_it = reduced_density_it
 
-# Compute true expectation.
+# Compute true expectation using mean of second half of all simulations.
 t0 = int(niterations/2)
 true_expectation = A_it[:,t0:].mean(1).mean(0)
 
-# TODO: Read this in?
-timestep = 0.002846993438 # ps
-nsteps_per_iteration = 25
-iterations_per_ns = timestep * nsteps_per_iteration * 1000 # ns per iteration # TODO: Fix me; inverse of what we want?
-print "iterations_per_ns = %f" % iterations_per_ns
+# Compute true expectation using all data. # DEBUG
+#true_expectation = A_it.mean().mean()
+#print "true expectation (from all data) : %f" % true_expectation
 
-# Write initial reduced densities.
-print "Initial reduced densities:"
-print A_it[:,0]
+#
+# BIAS-VARIANCE TRADEOFF CALCULATIONS
+#
 
 x = np.arange(niterations+1) / iterations_per_ns # ns
 A_t = A_it.mean(0)
 dA_t = A_it.std(0)
 
 # Compute integrated autocorrelation times.
-from pymbar import timeseries
-t0max = 500 # largest number of initial samples to discard
-tmax = 1000 # total trajectory length to include in analysis
-
-
-t0max = 1000 # largest number of initial samples to discard
-tmax = 2000 # total trajectory length to include in analysis
-
-print "tmax = %d (%.3f ns)" % (tmax, tmax / iterations_per_ns)
-
+print "Computing integrated autocorrelation times for all initial equilibration times t0..."
 g_it = np.zeros([nreplicates,t0max], np.float64)
 for replicate in range(nreplicates):
     print "replicate %d / %d..." % (replicate, nreplicates)
     for t0 in range(t0max):
-        #A_t_list = [ np.squeeze(A_it[replicate, t0:]) for replicate in range(nreplicates) ]
         A_t = np.squeeze(A_it[replicate, t0:tmax])
-        g_it[replicate,t0] = timeseries.statisticalInefficiency(A_t)
-
-print g_it
+        g_it[replicate,t0] = statisticalInefficiency(A_t)
 
 # Compute effective number of uncorrelated samples.
+print "Computing effective number of uncorrelated samples..."
 N_it = np.zeros([nreplicates,t0max], np.float64)
 for replicate in range(nreplicates):
     print "replicate %d / %d..." % (replicate, nreplicates)
     for t0 in range(t0max):
         N_it[replicate,t0] = (tmax-t0+1) / g_it[replicate,t0]
-        #N_it[replicate,t0] = (tmax-t0+1) # DEBUG
-
-print N_it
 
 # Determine optimal equilibration end (t0equil) by maximum number of effective samples (max of Neff).
 t0equil = np.argmax(N_it.mean(0))
 print "t0equil = %d" % t0equil
 
 # Determine individual t0equil points.
+print "Determining optimal equilibration times for each trajectory..."
 t0equil_i = np.zeros([nreplicates], np.int32)
 for replicate in range(nreplicates):
     t0equil_i[replicate] = np.argmax(N_it[replicate,:])
 
 # Determine mean as a function of initial t0.
-
 tbvmax = tmax - 200
 Aburnin_it = np.zeros([nreplicates, tbvmax], np.float64)
 Astderr_it = np.zeros([nreplicates, tbvmax], np.float64)
 for i in range(nreplicates):
     for t0 in range(tbvmax):
         A_t = np.squeeze(A_it[i, t0:tmax])
-        g = timeseries.statisticalInefficiency(A_t)
+        g = statisticalInefficiency(A_t)
         Aburnin_it[i,t0] = A_t.mean()
         Astderr_it[i,t0] = A_t.std() / np.sqrt((tmax-t0+1) / g)
 
@@ -120,10 +133,6 @@ print "%8s   %10s %10s" % ("", "bias", "stderr")
 for t0 in range(t0max):
     print "%8d : %10.6f %10.6f" % (t0, Abias_mean_t[t0], Astderr_mean_t[t0])
 print ""
-
-#
-#
-#
 
 # Compute estimates bias and variance using no equilibration, arbitrary equilibration, and optimal equilibration.
 bias_i = dict()
@@ -144,15 +153,15 @@ for replicate in range(nreplicates):
         else:
             raise Exception("scheme '%s' unknown" % scheme)
 
-        # Extract timeseries.
+        # Extract timeseries for desired equilibration time t0.
         A_t = np.squeeze(A_it[replicate, t0:tmax])
         N = len(A_t)
-        g = timeseries.statisticalInefficiency(A_t)
+        g = g_it[replicate, t0]
         # Compute statistics of estimate.
         bias_i[scheme][replicate] = A_t.mean() - true_expectation
         stddev_i[scheme][replicate] = A_t.std() / np.sqrt(N/g)
 
-# Compute statistics.
+# Summarize error, bias, and variance statistics for different equilibration time selection schemes.
 print ""
 print "STATISTICS"
 for scheme in schemes:
@@ -160,17 +169,16 @@ for scheme in schemes:
 print ""
 
 #
-# CREATE RMS ERROR FIGURE
+# FIGURE 4: RMS ERROR FOR FIXED EQUILIBRATION TIME VS AUTOMATIC EQUILIBRATION TIME SELECTION
 #
 
 print "Creating RMS error figure..."
 
 # Create plot as PDF.
-filename = os.path.join('figures', 'argon-rmse.pdf') # PDF file to write
+filename = os.path.join(figure_directory, 'argon-rmse.pdf') # PDF file to write
 
 x = np.arange(tbvmax) / iterations_per_ns # ns
 
-from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages(filename)
 
 import pylab
@@ -200,7 +208,6 @@ pylab.fill_between(x[0:tbvmax:nskip], rmse_t0[0:tbvmax:nskip]+2*drmse_t0[0:tbvma
 
 # Adjust axes.
 oldaxis = pylab.axis()
-#pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 oldaxis = (oldaxis[0], oldaxis[1], 0, 0.006)
 pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 
@@ -215,26 +222,20 @@ pylab.fill_between([x[0], x[tbvmax-1]], (rmse_optimal+2*drmse_optimal)*np.array(
 pylab.xlabel('$t_0$ / ns')
 pylab.ylabel('$\delta \hat{\mathrm{A}}$')
 
-#pylab.show()
 figure.tight_layout()
-
-#pylab.axis([-0.0005, 0.006, -0.0005, 0.006])
-#pylab.axes().set_aspect('equal', adjustable='box')
-#pylab.axes().set_xticks([0.0, 0.002, 0.004, 0.006])
-#pylab.axes().set_yticks([0.0, 0.002, 0.004, 0.006])
 
 # Write figure to PDF
 pp.savefig()
 pp.close()
 
 #
-# CREATE BIAS_VARIANCE FIGURE
+# FIGURE 3: CREATE BIAS_VARIANCE FIGURE
 #
 
 print "Creating bias-variance tradeoff figure..."
 
 # Create plot as PDF.
-filename = os.path.join('figures', 'argon-bias-variance.pdf') # PDF file to write
+filename = os.path.join(figure_directory, 'argon-bias-variance.pdf') # PDF file to write
 
 from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages(filename)
@@ -254,18 +255,19 @@ figure = pylab.figure(figsize=(3.5, 3.5), dpi=300)
 nskip = 10 # number of samples to skip in uncertainty shading
 hspace = 0.001 # spacing between subplots
 
+from matplotlib.colors import Normalize
+framelist = range(0, tbvmax)
+norm = Normalize(framelist) # normalized value of frame number
+from matplotlib.cm import ScalarMappable
 ax = figure.add_subplot(1, 1, 1)
-cmap = pylab.cm.rainbow(np.linspace(1, 0, tbvmax))
-sc = ax.scatter(Abias_mean_t, Astderr_mean_t, np.linspace(1, 0, tbvmax), cmap=cmap)
+# Create ranbow colormap
+cmap = plt.cm.get_cmap('rainbow')
+sc = ax.scatter(Abias_mean_t, Astderr_mean_t, c=framelist, vmin=0, vmax=tbvmax, s=20, cmap=cmap, linewidths=0)
 pylab.hold(True)
-#pylab.plot(bias_i['optequil'].mean(),  stddev_i['optequil'].mean(), 'kv')
 pylab.plot(bias_i['optequil'].mean(),  bias_i['optequil'].std(), 'kv')
 
 # Add colorbar, make sure to specify tick locations to match desired ticklabels
-#from matplotlib.colorbar import ColorbarBase
-#cbar = ColorbarBase(ax, cmap=colors, ticks=[0, niterations])
-#cbar.ax.set_yticklabels(['0 ns', '%.0f ns' % float(niterations/iterations_per_ns)]) # vertically oriented colorbar
-pylab.colorbar(sc)
+pylab.colorbar(sc, orientation='vertical', shrink=0.70)
 
 # Label plot
 pylab.xlabel('bias')
@@ -284,23 +286,16 @@ pp.savefig()
 pp.close()
 
 #
-# CREATE REVERSE CUMULATIVE AVERAGE FIGURE
+# FIGURE 2: STATISTICAL INEFFICIENCY, NUMBER OF UNCORRELATED SAMPLES, AND BIAS AS A FUNCTION OF FIXED EQUILIBRATION TIME
 #
 
 print "Creating reverse cumulative averages figure..."
 
 # Create plot as PDF.
-filename = os.path.join('figures', 'argon-reverse-cumulative-average.pdf') # PDF file to write
+filename = os.path.join(figure_directory, 'argon-reverse-cumulative-average.pdf') # PDF file to write
 
-from matplotlib.backends.backend_pdf import PdfPages
 pp = PdfPages(filename)
 
-import pylab
-#import seaborn as sns
-
-#
-# FIGURE
-#
 
 fontsize=10
 
@@ -322,33 +317,21 @@ hspace = 0.001 # spacing between subplots
 subplot = pylab.subplot(311)
 pylab.subplots_adjust(hspace=hspace)
 pylab.hold(True)
-#pylab.fill_between(x[:nmax:nskip], A_t[:nmax:nskip]+2*dA_t[:nmax:nskip], A_t[:nmax:nskip]-2*dA_t[:nmax:nskip], facecolor='grey', edgecolor='grey', alpha=0.5, linewidth=0)
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, g_it[:,:t0max].T / iterations_per_ns, 'k-')
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, N_it[:,:t0max].T / iterations_per_ns, 'k-')
-
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, N_it[:,:t0max].T, 'k-')
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, g_it[:,:t0max].T, 'r-')
 
 pylab.fill_between(x[0:t0max:nskip], g_it[:,0:t0max:nskip].mean(0)+g_it[:,0:t0max:nskip].std(0), g_it[:,0:t0max:nskip].mean(0)-g_it[:,0:t0max:nskip].std(0), facecolor='gray', edgecolor='gray', alpha=0.5, linewidth=0)
 pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, g_it[:,:t0max].mean(0), 'k-')
 
-#pylab.xlabel('initial simulation time $t_0$ / ns')
 subplot.set_xticks([]) # no x-tick labels
 
-#pylab.ylabel(r'statistical inefficiency $g_[t_0,T]$ / samples', fontsize=fontsize)
-#pylab.ylabel(r'$g_[t_0,T]$ / ns', fontsize=fontsize)
 pylab.ylabel(r'$g$ / iterations', fontsize=fontsize)
 
 # Adjust axes.
 oldaxis = pylab.axis()
-#pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 oldaxis = (oldaxis[0], oldaxis[1], 0, 125)
 pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 
 # Plot t0equil.
 pylab.plot(x[t0equil]*np.array([1,1]), [oldaxis[2], oldaxis[3]], 'r-', linewidth=3)
-
-#subplot.set_xticks([]) # no x-tick labels
 
 subplot.set_yticks([0, 25, 50, 75, 100, 125]) # no x-tick labels
 
@@ -359,26 +342,16 @@ subplot.set_yticks([0, 25, 50, 75, 100, 125]) # no x-tick labels
 subplot = pylab.subplot(312)
 pylab.subplots_adjust(hspace=hspace)
 pylab.hold(True)
-#pylab.fill_between(x[:nmax:nskip], A_t[:nmax:nskip]+2*dA_t[:nmax:nskip], A_t[:nmax:nskip]-2*dA_t[:nmax:nskip], facecolor='grey', edgecolor='grey', alpha=0.5, linewidth=0)
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, g_it[:,:t0max].T / iterations_per_ns, 'k-')
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, N_it[:,:t0max].T / iterations_per_ns, 'k-')
-
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, N_it[:,:t0max].T, 'k-')
-#pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, g_it[:,:t0max].T, 'r-')
 
 pylab.fill_between(x[0:t0max:nskip], N_it[:,0:t0max:nskip].mean(0)+N_it[:,0:t0max:nskip].std(0), N_it[:,0:t0max:nskip].mean(0)-N_it[:,0:t0max:nskip].std(0), facecolor='gray', edgecolor='gray', alpha=0.5, linewidth=0)
 pylab.plot(np.tile(x[0:t0max], [nreplicates,1]).T, N_it[:,:t0max].mean(0), 'k-')
 
-#pylab.xlabel('initial simulation time $t_0$ / ns')
 subplot.set_xticks([]) # no x-tick labels
 
-#pylab.ylabel(r'statistical inefficiency $g_[t_0,T]$ / samples', fontsize=fontsize)
-#pylab.ylabel(r'$g_[t_0,T]$ / ns', fontsize=fontsize)
 pylab.ylabel(r'$N_\mathrm{eff}$', fontsize=fontsize)
 
 # Adjust axes.
 oldaxis = pylab.axis()
-#pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 oldaxis = (oldaxis[0], oldaxis[1], 0, 50)
 pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 
@@ -396,17 +369,12 @@ subplot.set_yticks([0, 10, 20, 30, 40, 50])
 # BOTTOM PANEL: ERROR AS A FUNCTION OF BURN-IN PERIOD
 #
 
-#print "Cumulative average:"
-#print "%8s %8s %8s" % ('t / ps', 'Acum', 'stdAcum')
-#for t in range(100):
-#    print "%8.1f %8.5f %8.5f" % (t*nsteps_per_iteration*timestep, Acumavg_mean_t[t], Acumavg_std_t[t])
-
 subplot = pylab.subplot(313)
 pylab.subplots_adjust(hspace=hspace)
 pylab.hold(True)
 
 pylab.fill_between(x[0:t0max:nskip], Aburnin_mean_t[0:t0max:nskip]+Aburnin_std_t[0:t0max:nskip], Aburnin_mean_t[0:t0max:nskip]-Aburnin_std_t[0:t0max:nskip], facecolor='gray', edgecolor='gray', alpha=0.5, linewidth=0)
-pylab.plot([x[0], x[t0max]], [true_expectation, true_expectation], 'r:')
+pylab.plot([x[0], x[t0max]], [true_expectation, true_expectation], 'r--')
 pylab.plot(x[0:t0max], Aburnin_mean_t[0:t0max], 'k-')
 
 pylab.legend(['true expectation', 'discarding initial $[0,t_0]$'], fontsize=fontsize-2, frameon=False)
@@ -414,11 +382,8 @@ pylab.legend(['true expectation', 'discarding initial $[0,t_0]$'], fontsize=font
 pylab.xlabel('initial simulation time $t_0$ / ns')
 pylab.ylabel(r'$\left\langle\rho^*\right\rangle_{[t_0,T]}$', fontsize=fontsize)
 
-#subplot.set_yticks([0.7, 0.8, 0.9, 1.0])
-
 # Adjust axes.
 oldaxis = pylab.axis()
-#pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 oldaxis = (oldaxis[0], oldaxis[1], 0.778, 0.794)
 pylab.axis([0, x[:t0max].max(), oldaxis[2], oldaxis[3]])
 
